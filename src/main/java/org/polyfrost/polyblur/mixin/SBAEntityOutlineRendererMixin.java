2ffeef1c1711c5a0f7b6f71e6e16979175093102
ev chang <wyvestbusiness@gmail.com>
2024-07-01 06:20:43 +0900

    Fix injectors for SBA / SkyHanni compat mixins

--- src/main/java/org/polyfrost/polyblur/mixin/SBAEntityOutlineRendererMixin.java
+++ src/main/java/org/polyfrost/polyblur/mixin/SBAEntityOutlineRendererMixin.java
@@ -1,12 +1,13 @@
 package org.polyfrost.polyblur.mixin;
 
+import net.minecraft.client.renderer.culling.ICamera;
 import org.polyfrost.polyblur.blurs.monkey.MonkeyBlur;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
 @Mixin(targets = "codes.biscuit.skyblockaddons.features.EntityOutlines.EntityOutlineRenderer")
@@ -14,7 +15,7 @@ public class SBAEntityOutlineRendererMixin {
 
     @Dynamic("SBA")
     @Inject(method = "renderEntityOutlines", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/shader/Framebuffer;bindFramebuffer(Z)V", ordinal = 3, shift = At.Shift.AFTER, remap = true), remap = false)
-    private void onRenderEntityOutlinesPre(CallbackInfo ci) {
+    private static void onRenderEntityOutlinesPre(ICamera camera, float partialTicks, double x, double y, double z, CallbackInfoReturnable<Boolean> ci) {
         MonkeyBlur.instance.bindFb();
     }
 }
