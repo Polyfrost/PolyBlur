7f72c8d4887a6e4f3d86ee310100c54aa0ebd321
Deftu <me@deftu.dev>
2024-10-18 18:23:01 +0200

    Fix command not opening config UI

--- src/main/java/org/polyfrost/polyblur/commands/PolyBlurCommand.java
+++ src/main/java/org/polyfrost/polyblur/commands/PolyBlurCommand.java
@@ -2,13 +2,14 @@ package org.polyfrost.polyblur.commands;
 
 import org.polyfrost.oneconfig.api.commands.v1.factories.annotated.Command;
 import org.polyfrost.polyblur.PolyBlur;
+import org.polyfrost.utils.v1.dsl.ScreensKt;
 
 @Command("polyblur")
 public class PolyBlurCommand {
 
     @Command
-    public static void handle() {
-        // TODO: Implement openGui
-        // PolyBlur.instance.config.openGui();
+    public void main() {
+        ScreensKt.openUI(PolyBlur.instance.config);
     }
+
 }
