a0c6e4655efab424707df9afe391160feec1c6ef
Deftu <me@deftu.dev>
2025-01-02 20:30:40 +0200

    Bring up to the speed with the OneConfig example mod

--- root.gradle.kts
+++ root.gradle.kts
@@ -3,11 +3,20 @@ plugins {
 }
 
 preprocess {
-    "1.12.2-forge"(11202, "srg") {
-        "1.12.2-fabric"(11202, "yarn", rootProject.file("versions/1.12.2-1.8.9.txt")) {
-            "1.8.9-fabric"(10809, "yarn") {
-                "1.8.9-forge"(10809, "srg")
+    // Adding new versions/loaders can be done like so:
+    // For each version, we add a new wrapper around the last from highest to lowest.
+    // Each mod loader needs to link up to the previous version's mod loader so that the mappings can be processed from the previous version.
+    // "1.12.2-forge"(11202, "srg") {
+    //     "1.8.9-forge"(10809, "srg")
+    // }
+
+    "1.12.2-fabric"(11202, "yarn") {
+        "1.12.2-forge"(11202, "srg") {
+            "1.8.9-forge"(10809, "srg", rootProject.file("versions/1.12.2-1.8.9.txt")) {
+                "1.8.9-fabric"(10809, "yarn")
             }
         }
     }
+
+    strictExtraMappings.set(true)
 }
